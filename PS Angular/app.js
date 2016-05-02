12c52025891aa0640a1858fc7a0c6b2125627407
Fahad Qazi <fahadqazi@gmail.com>
2016-05-02 16:30:36 +0100

    final changes

--- PS Angular/app.js
+++ PS Angular/app.js
@@ -3,10 +3,9 @@
     var app = angular.module("githubViewer", []);
 //    var app = angular.module("githubViewer", []);
     
-    var MainController = function($scope, $http){
-      
-//      $scope.username = "fahadqazi";
-      $scope.message = "Hello Angular";
+    var MainController = function(
+      $scope, $http, $interval, 
+      $anchorScroll, $location){
         
       var url = "https://api.github.com/users/";
       
@@ -18,6 +17,8 @@
       
       var onRepos = function(response){
         $scope.repos = response.data;
+        $location.hash("userDetails");
+        $anchorScroll();
       }
       
       var onError = function(reason){
@@ -27,7 +28,29 @@
       $scope.search = function(username){
         $http.get(url+username)
         .then(onUserComplete, onError);
+        if(countDownInterval){
+          $interval.cancel(countDownInterval);
+          $scope.countdown = null;
+        }
       };
+      
+      var decrementCountdown = function(){
+        $scope.countdown -= 1;
+        if($scope.countdown < 1){
+          $scope.search($scope.username);
+        }
+      };
+      
+      var countDownInterval = null;
+      var startCountdown = function(){
+        countDownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+      };
+      
+      $scope.username = "angular";
+      $scope.message = "Hello Angular";
+      $scope.repoSortOrder = "-startgazers_count";
+      $scope.countdown = 5;
+      startCountdown();
     
     };
     
